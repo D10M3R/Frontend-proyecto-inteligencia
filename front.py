301c2a3012dd1d25a49535ba573c199521c4738d
Diomer <diomersandoval@gmail.com>
2024-05-29 16:37:19 -0500

    agregar enlace de uso de la api

--- front.py
+++ front.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import locale
 
-API_URL = 'http://127.0.0.1:8000/predict/'
+API_URL = 'https://api-rest-ia.onrender.com/predict/'
 
 def make_prediction(data):
     response = requests.post(API_URL, json=data)
